bbc1e22c8a201537411aa86a6f1dd132092959ea
warin <zhuboyuan2020@gmail.com>
2022-04-23 04:59:56 +0800

    add morgan logger at backend

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const path = require('path')
+const morgan = require('morgan')
 const router = require('./router/index')
 const init = require('./db/init')
 const bodyParser = require('body-parser');
@@ -9,6 +10,7 @@ init(reset)
 
 const app = express()
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
 
